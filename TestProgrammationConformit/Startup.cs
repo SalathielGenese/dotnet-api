044dc00df5b50cec9d08d45a6135af82dc805bab
Salathiel Genese <salathielgenese@gmail.com>
2021-04-14 23:58:33 +0400

    fix: optimize imports

--- TestProgrammationConformit/Startup.cs
+++ TestProgrammationConformit/Startup.cs
@@ -1,16 +1,10 @@
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using TestProgrammationConformit.Infrastructures;
 
 namespace TestProgrammationConformit
