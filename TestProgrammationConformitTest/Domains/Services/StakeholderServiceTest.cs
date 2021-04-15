2798cf92848663563e576d49612c1db4ca30c908
Salathiel Genese <salathielgenese@gmail.com>
2021-04-16 02:24:42 +0400

    feat: more test cases around the StakeholderService#Persist with DTO

--- TestProgrammationConformitTest/Domains/Services/StakeholderServiceTest.cs
+++ TestProgrammationConformitTest/Domains/Services/StakeholderServiceTest.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using NUnit.Framework;
 using TestProgrammationConformit.Domains.Models;
 using TestProgrammationConformit.Domains.Services;
@@ -20,6 +21,29 @@ namespace TestProgrammationConformitTest.Domains.Services
         {
             var stakeholder = _stakeholderService.Persist(new Stakeholder {Name = "Shaban"});
             Assert.IsInstanceOf(typeof(Stakeholder), stakeholder);
+        }
+
+        [Test]
+        public void Persist_WithStakeholderDto_RecordASingleStakeholder()
+        {
+            _stakeholderService.Persist(new Stakeholder {Name = "Shaban"});
+            var count = ConformitContext.Stakeholders.Count();
+            Assert.AreEqual(count, 1);
+        }
+
+        [Test]
+        public void Persist_WithStakeholderDto_ReturnsAStakeholderWithSameName()
+        {
+            var name = "Shaban";
+            var stakeholder = _stakeholderService.Persist(new Stakeholder {Name = name});
+            Assert.AreEqual(stakeholder?.Name, name);
+        }
+
+        [Test]
+        public void Persist_WithStakeholderDto_ReturnsAStakeholderWithNonNullId()
+        {
+            var stakeholder = _stakeholderService.Persist(new Stakeholder {Name = "Shaban"});
+            Assert.AreNotEqual(stakeholder?.Id, 0);
         }
     }
 }
