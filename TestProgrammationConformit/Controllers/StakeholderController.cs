15e722b726c7108890606a58dfca11109b11403e
Salathiel Genese <salathielgenese@gmail.com>
2021-04-15 13:05:05 +0400

    feat: have StakeholderController rely of BaseController abstraction

--- TestProgrammationConformit/Controllers/StakeholderController.cs
+++ TestProgrammationConformit/Controllers/StakeholderController.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using TestProgrammationConformit.Domains.Models;
 using TestProgrammationConformit.Domains.Services;
@@ -7,39 +6,10 @@ namespace TestProgrammationConformit.Controllers
 {
     [ApiController]
     [Route("/stakeholders")]
-    public class StakeholderController : Controller
+    public class StakeholderController : BaseController<Stakeholder, int>
     {
-        public StakeholderController(IService<Stakeholder, int> service)
+        public StakeholderController(IService<Stakeholder, int> service) : base(service, 0)
         {
-            Service = service;
-        }
-
-        private IService<Stakeholder, int> Service { get; }
-
-        [HttpGet]
-        public ActionResult<IEnumerable<Stakeholder>> Index() => Ok(Service.Find(2, 5));
-
-        [HttpDelete("{id:int}")]
-        public ActionResult<Stakeholder> Delete([FromRoute] int id) => Service.Delete(id) ? NotFound() : NoContent();
-
-        [HttpPost]
-        [HttpPut("{id:int}")]
-        public ActionResult<Stakeholder> Persist([FromRoute] int id, [FromBody] Stakeholder stakeholder)
-        {
-            if (0 != id)
-            {
-                stakeholder.Id = id;
-            }
-
-            stakeholder = Service.Persist(stakeholder);
-            return null == stakeholder ? NotFound() : Ok(stakeholder);
-        }
-
-        [HttpGet("{id:int}")]
-        public ActionResult<Stakeholder> Details([FromRoute] int id)
-        {
-            var stakeholder = Service.Find(id);
-            return null == stakeholder ? NotFound() : Ok(stakeholder);
         }
     }
 }
