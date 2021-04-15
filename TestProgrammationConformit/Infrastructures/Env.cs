adc2b2b636bae94b509740fd94dfaf23ff90a3a0
Salathiel Genese <salathielgenese@gmail.com>
2021-04-15 14:44:18 +0400

    feat: extract minimum page index and page size from environment

--- TestProgrammationConformit/Infrastructures/Env.cs
+++ TestProgrammationConformit/Infrastructures/Env.cs
@@ -1,4 +1,5 @@
 using System;
+using TestProgrammationConformit.Infrastructures.Http;
 
 namespace TestProgrammationConformit.Infrastructures
 {
@@ -6,9 +7,21 @@ namespace TestProgrammationConformit.Infrastructures
     {
         public Env()
         {
+            int page, size;
+            var pageString = Environment.GetEnvironmentVariable("ConnectionStrings__page") ?? string.Empty;
+            var sizeString = Environment.GetEnvironmentVariable("ConnectionStrings__size") ?? string.Empty;
+
+            int.TryParse(pageString, out page);
+            int.TryParse(sizeString, out size);
+            page = Math.Max(1, page);
+            size = Math.Max(1, size);
+            Pageable = new Pageable {Page = page, Size = size};
+
             PostgresConnectionString =
                 Environment.GetEnvironmentVariable("ConnectionStrings__ConformitDb") ?? string.Empty;
         }
+
+        public Pageable Pageable { get; }
         public string PostgresConnectionString { get; }
     }
 }
