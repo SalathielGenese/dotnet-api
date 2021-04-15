86849d1a72b2eb558cb6e95d413c4d90a841cd1f
Salathiel Genese <salathielgenese@gmail.com>
2021-04-16 02:13:23 +0400

    feat: start service testing

---
+++ TestProgrammationConformitTest/Domains/Services/StakeholderServiceTest.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+using TestProgrammationConformit.Domains.Models;
+using TestProgrammationConformit.Domains.Services;
+
+namespace TestProgrammationConformitTest.Domains.Services
+{
+    public class StakeholderServiceTest : BaseServiceTest
+    {
+        private StakeholderService _stakeholderService;
+
+        [SetUp]
+        public new void Setup()
+        {
+            base.Setup();
+            _stakeholderService = new StakeholderService(ConformitContext, ConformitContext.Stakeholders, 0);
+        }
+
+        [Test]
+        public void Persist_ReturnsAStakeholder_WithNoIdStakeholder()
+        {
+            var stakeholder = _stakeholderService.Persist(new Stakeholder {Name = "Shaban"});
+            Assert.IsInstanceOf(typeof(Stakeholder), stakeholder);
+        }
+    }
+}
