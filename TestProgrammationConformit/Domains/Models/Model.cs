daa4e57ad39d35dd731b0ed9a88816f1e45d4aca
Salathiel Genese <salathielgenese@gmail.com>
2021-04-16 13:29:18 +0400

    feat: track models changes

--- TestProgrammationConformit/Domains/Models/Model.cs
+++ TestProgrammationConformit/Domains/Models/Model.cs
@@ -1,7 +1,12 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace TestProgrammationConformit.Domains.Models
 {
     public abstract class Model<TId>
     {
         public abstract TId Id { get; set; }
+        [Required] public DateTime CreatedAt { get; set; }
+        [Required] public DateTime UpdatedAt { get; set; }
     }
 }
