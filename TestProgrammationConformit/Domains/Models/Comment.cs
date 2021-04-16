2da7ce1a975dc734796701cd1298d2ec276a97de
Salathiel Genese <salathielgenese@gmail.com>
2021-04-16 23:25:33 +0400

    feat: rearrange comment entity

--- TestProgrammationConformit/Domains/Models/Comment.cs
+++ TestProgrammationConformit/Domains/Models/Comment.cs
@@ -1,15 +1,13 @@
-using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace TestProgrammationConformit.Domains.Models
 {
-    public class Comment
+    public class Comment : Model<int>
     {
-        public int Id { get; set; }
+        public override int Id { get; set; }
         [Required] public int EventId { get; set; }
         [Required] public int StakeholderId { set; get; }
 
-        [Required] public DateTime DateTime { get; set; }
         [Required] public string Description { get; set; }
     }
 }
