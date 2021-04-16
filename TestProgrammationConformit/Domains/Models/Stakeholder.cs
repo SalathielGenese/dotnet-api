445789ce71abcba77952809095b4b7332864f931
Salathiel Genese <salathielgenese@gmail.com>
2021-04-16 04:01:39 +0400

    feat: add unique constraint to Stakeholder#Name

--- TestProgrammationConformit/Domains/Models/Stakeholder.cs
+++ TestProgrammationConformit/Domains/Models/Stakeholder.cs
@@ -1,7 +1,9 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace TestProgrammationConformit.Domains.Models
 {
+    [Index(nameof(Name), IsUnique = true)]
     public class Stakeholder: Model<int>
     {
         public override int Id { get; set; }
---
+++ TestProgrammationConformit/Migrations/20210415235507_AddConstraintStakeholderNameUniqueIndex.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestProgrammationConformit.Migrations
+{
+    public partial class AddConstraintStakeholderNameUniqueIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Stakeholders_Name",
+                table: "Stakeholders",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Stakeholders_Name",
+                table: "Stakeholders");
+        }
+    }
+}
