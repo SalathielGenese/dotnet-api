57bdd3b50a230dac8cba5d7c85b0f3f2ea6a2b50
Salathiel Genese <salathielgenese@gmail.com>
2021-04-16 13:33:42 +0400

    feat: generate migration with entity framework
    ConnectionStrings__ConformitDb=<connection-string> \
      dotnet ef migrations add TrackModelChanges

---
+++ TestProgrammationConformit/Migrations/20210416092831_TrackModelChanges.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestProgrammationConformit.Migrations
+{
+    public partial class TrackModelChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateTime",
+                table: "Events",
+                newName: "UpdatedAt");
+
+            migrationBuilder.RenameColumn(
+                name: "DateTime",
+                table: "Comments",
+                newName: "UpdatedAt");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Stakeholders",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Stakeholders",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Events",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Comments",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Stakeholders");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Stakeholders");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Comments");
+
+            migrationBuilder.RenameColumn(
+                name: "UpdatedAt",
+                table: "Events",
+                newName: "DateTime");
+
+            migrationBuilder.RenameColumn(
+                name: "UpdatedAt",
+                table: "Comments",
+                newName: "DateTime");
+        }
+    }
+}
