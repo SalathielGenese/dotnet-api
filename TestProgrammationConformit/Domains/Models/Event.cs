b705f8aa741c4c6f54f960b5690958562adca4d7
Salathiel Genese <salathielgenese@gmail.com>
2021-04-16 23:01:23 +0400

    feat: rearrange event properties

--- TestProgrammationConformit/Domains/Models/Event.cs
+++ TestProgrammationConformit/Domains/Models/Event.cs
@@ -1,18 +1,17 @@
-using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace TestProgrammationConformit.Domains.Models
 {
-    public class Event
+    public class Event : Model<int>
     {
-        public int Id { get; set; }
+        public override int Id { get; set; }
         [Required] public int StakeholderId { set; get; }
 
-        [Required] public DateTime DateTime { get; set; }
-        public Stakeholder Stakeholder { get; set; }
         [Required] public string Description { get; set; }
         [Required] [MaxLength(100)] public string Title { get; set; }
+
+        public Stakeholder Stakeholder { get; set; }
         public List<Comment> Comments { get; set; } = new List<Comment>();
     }
 }
